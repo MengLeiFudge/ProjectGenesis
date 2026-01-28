30a20bcf81a9147b183f054e52366aa5b0120a4a
萌泪酱 <605738729@qq.com>
2026-01-28 15:42:50 +0800

    PostAdd后也执行InitRecipeItems
    (cherry picked from commit 1ccaba631d5c8b22df8474221c9f8f243b8571e2)

--- src/ProjectGenesis.cs
+++ src/ProjectGenesis.cs
@@ -291,6 +291,7 @@ namespace ProjectGenesis
             ModelProto.InitMaxModelIndex();
             ModelProto.InitModelIndices();
             ModelProto.InitModelOrders();
+            RecipeProto.InitRecipeItems();
             RecipeProto.InitFractionatorNeeds();
             SignalProtoSet.InitSignalKeyIdPairs();
             RaycastLogic.LoadStatic();
